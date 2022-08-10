d55cc4312d33be2ef5844725e2c4314ceca0b834
Vlad Harkusha <sivari888@gmail.com>
2022-08-10 18:38:07 +0300

    create buttons and styles

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className='app'>
+      <div className='buttons'>
+        <button className='buttons__button'>Top up</button>
+        <button className='buttons__button'>Withdraw</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
