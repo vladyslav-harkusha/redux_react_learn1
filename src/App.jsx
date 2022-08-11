c1ae84c7976c3e4e8a58130acad245d3440aa324
Vlad Harkusha <sivari888@gmail.com>
2022-08-11 16:12:52 +0300

    add store and combine reducers

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import './App.scss';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
+  const dispatch = useDispatch();
+  const cash = useSelector(state => state.cashR.cash);
+
+  const addCash = (cashToAdd) => {
+    dispatch({ type: 'ADD_CASH', payload: cashToAdd })
+  };
+
+  const getCash = (cashToGet) => {
+    dispatch({ type: 'GET_CASH', payload: cashToGet })
+  };
+
   return (
     <div className='app'>
       <div className='buttons'>
-        <button className='buttons__button'>Top up</button>
-        <button className='buttons__button'>Withdraw</button>
+        <p className="buttons__cash">{cash}</p>
+
+        <button onClick={() => addCash(+prompt())} className='buttons__button'>Add cash</button>
+        
+        <button onClick={() => getCash(+prompt())} className='buttons__button'>Get cash</button>
       </div>
     </div>
   );
