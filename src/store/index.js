b1b9772645c132d27e48a6a14fa4e8d6a1dcd8d5
Vlad Harkusha <sivari888@gmail.com>
2022-08-11 16:31:25 +0300

    install redux-devtools-extension

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import { createStore, combineReducers } from 'redux';
 import { cashReducer } from './cashReducer';
 import { customerReducer } from './customerReducer';
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const rootReducer = combineReducers({
   cashR: cashReducer,
   customerR: customerReducer,
 });
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, composeWithDevTools());
